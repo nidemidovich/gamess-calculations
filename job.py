52212a792e8f6e958ec380c97ebf94d7653ab02b
Nikita <demidovicnikita1@gmail.com>
2021-10-08 13:26:48 +0300

    Added creation of reports from the Job class

--- job.py
+++ job.py
@@ -4,12 +4,13 @@ from utils import gamout_to_mol, smiles_to_mol
 
 class Job:
 
-    def __init__(self, material, material_name, config, opt_params, nrg_params):
+    def __init__(self, material, material_name, config, opt_params, nrg_params, report):
         self.material = material
         self.material_name = material_name
         self.config = config
         self.opt_params = opt_params
         self.nrg_params = nrg_params
+        self.report = report
 
     def job(self):
         gamess = Gamess(
@@ -47,3 +48,5 @@ class Job:
             ncpus=self.config['ncpus'],
             output_file=energy_output_file
         )
+
+        self.report.get_report(energy_output_file)
--- report.py
+++ report.py
@@ -4,22 +4,19 @@ import json
 class BaseReport:
     def __init__(self, result, file_to_report='report.txt', smiles='smiles.json'):
         """
-        :param result: file with results of the calculations
-        :type result: str
         :param file_to_report: file to write the report
         :type file_to_report: str
         :param smiles: path to file with the SMILES materials
         :type smiles: str
         """
-        self.result = result
         self.file_to_report = file_to_report
         self.smiles = smiles
 
-    def get_report(self):
-        name = self.result.split('_')[0]
+    def get_report(self, result):
+        name = result.split('_')[0]
         material = self.get_material(name)
 
-        with open(self.file_to_report, 'a+') as report, open(self.result, encoding='utf-8') as result_reader:
+        with open(self.file_to_report, 'a+') as report, open(result, encoding='utf-8') as result_reader:
             report.write(material)
             report.write('\n')
             for line in result_reader:
@@ -34,11 +31,11 @@ class BaseReport:
 
 
 class TransitionStateReport(BaseReport):
-    def get_report(self):
-        name = self.result.split('_')[0]
+    def get_report(self, result):
+        name = result.split('_')[0]
         material = self.get_material(name)
 
-        with open(self.result, encoding='utf-8') as result_reader:
+        with open(result, encoding='utf-8') as result_reader:
             line = result_reader.readline()
             while 'SUMMARY OF TDDFT RESULTS' not in line:
                 line = result_reader.readline()
